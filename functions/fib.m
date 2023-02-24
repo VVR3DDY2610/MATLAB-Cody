00758e637a36249c731afca9b405d642a510aec0
VISHNU VARDHAN REDDY BAKKA <107259404+VVR3DDY2610@users.noreply.github.com>
2023-02-24 12:24:41 +0530

    Update fib.m

--- functions/fib.m
+++ functions/fib.m
@@ -1,18 +1,29 @@
 function f = fib(n)
-  
-  sequence = zeros(1,n);
-  sequence(1,1) = 1;
-  sequence(1,2) = 1;
-  
-  if n == 1
-      f = 1;
-  elseif n == 2
-      f = 1;
-  else 
-      for i = 3:n
-          sequence(1,i) = sequence(1,i-1) + sequence(1,i-2);
-      end
-      f = sequence(1,n);
-  end
+
+  f(1) = 1;  % We have to prime our code with the first two values of fib
+
+  f(2) = 1;  % as we start the loop at f(3) = f(1) + f(2), not really cheating
+
+             % and it makes f true for values of n=1 or 2 (robustness).
+
+    for i = 2 : n-1
+
+      f(i+1) = f(i-1) + f(i);  % For optimization sake, we should have prelocated
+
+                               % f with f = zeros(n). For our sake, we care
+
+                               % about size and not about speed so I
+
+                               % didn't. We save each value of the fib to
+
+                               % an indexed value within f to not only
+
+                               % calculate the next number but to speed it
+
+                               % up as well (eliminates repeat calculation)
+
+    end
+
+    f = f(n);
 
 end
